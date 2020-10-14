e283d3599e50449aef9836091a8a40887c4c1e8f
Musical Allegory <exbuildsthings@outlook.com>
2020-10-14 20:19:08 +0100

    changed text for another demo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div style={{ color: `purple`, fontSize: `72px` }}>Hello Mum!</div>
+  return <div style={{ color: `pink`, fontSize: `72px` }}>Hello Miss Zara!<br> behold the stinging butterfly bee...</br></div>
 }
