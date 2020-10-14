5872823c4737d0b7c433b5b833ec1326c5054645
Musical Allegory <exbuildsthings@outlook.com>
2020-10-14 20:30:59 +0100

    try to fix error, add ignore file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div style={{ color: `pink`, fontSize: `72px` }}>Hello Miss Zara!<br> behold the stinging butterfly bee...</br></div>
+  return <div style={{ color: `pink`, fontSize: `72px` }}>Hello Miss Zara! behold the stinging butterfly bee...</div>
 }
